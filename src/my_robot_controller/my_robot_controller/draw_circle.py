84dbdbf31afe3801075fe90ca2f15b3b116eb5ba
Beren Akpinar <akpinarbe22@gmail.com>
2025-06-06 13:58:59 -0400

    including src

---
+++ src/my_robot_controller/my_robot_controller/draw_circle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class DrawCircleNode(Node):
+
+    def __init__(self):
+        super().__init__("draw_circle")
+        self.cmv_vel_pub_ = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
+        self.get_logger().info("Draw circle node has been started")
+    
+def main(args=None):
+    rclpy.init(args=args)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
